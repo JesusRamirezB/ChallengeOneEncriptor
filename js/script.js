8c7ce44e2bf1a5f574cfa2ba8f4c1295734aa365
JesusBora <jramirezb2000@gmail.com>
2023-04-12 16:13:29 -0600

    Updated encrypt method & added copy button

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const textInput = document.getElementById('text-input')
 const encryptButton = document.getElementById('encrypt')
 const decryptButton = document.getElementById('decrypt')
 const textOutput = document.getElementById('text-modified')
-const copyright = document.getElementById("copyright");
-const caracters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '];
+const copyright = document.getElementById('copyright');
+const copyButton = document.getElementById('copy-button')
 const currentYear = new Date().getFullYear();
 
 copyright.innerHTML = `&copy; ${currentYear}`;
@@ -11,34 +11,77 @@ copyright.innerHTML = `&copy; ${currentYear}`;
 function encryptText(text) {
     let result = ''
     Array.from(text).forEach(letter => {
-        if (letter == '\n') {
-            result += '<br>'
-        } else {
-            result += caracters[(caracters.indexOf(letter.toUpperCase()) + 10) % caracters.length]
+        switch (letter) {
+            case 'e':
+                result += 'enter';
+                break;
+            case 'i':
+                result += 'imes';
+                break;
+            case 'a':
+                result += 'ai';
+                break;
+            case 'o':
+                result += 'ober';
+                break;
+            case 'u':
+                result += 'ufat';
+                break;
+            default:
+                result += letter;
+                break;
         }
     })
     return result
 }
 
 function decryptText(text) {
-    let result = ''
-    Array.from(text).forEach(letter => {
-        if (letter == '\n') {
-            result += '<br>'
+    let desencriptada = '';
+    let i = 0;
+    while (i < text.length) {
+        if (text[i] === 'e' && text.slice(i, i + 5) === 'enter') {
+            desencriptada += 'e';
+            i += 5;
+        } else if (text[i] === 'i' && text.slice(i, i + 4) === 'imes') {
+            desencriptada += 'i';
+            i += 4;
+        } else if (text[i] === 'a' && text.slice(i, i + 2) === 'ai') {
+            desencriptada += 'a';
+            i += 2;
+        } else if (text[i] === 'o' && text.slice(i, i + 4) === 'ober') {
+            desencriptada += 'o';
+            i += 4;
+        } else if (text[i] === 'u' && text.slice(i, i + 4) === 'ufat') {
+            desencriptada += 'u';
+            i += 4;
         } else {
-            result += caracters[(caracters.indexOf(letter.toUpperCase()) + caracters.length - 10) % caracters.length]
+            desencriptada += text[i];
+            i++;
         }
-    })
-    return result
+    }
+    return desencriptada;
 }
 
-encryptButton.addEventListener("click", function () {
-    const text = textInput.value
+copyButton.addEventListener('click', function () {
+    const textoOculto = document.createElement("textarea");
+    textoOculto.value = textOutput.textContent;
+    textOutput.appendChild(textoOculto);
+    // Selecciona y copia el contenido del elemento de texto oculto
+    textoOculto.select();
+    document.execCommand("copy");
+    // Elimina el elemento de texto oculto
+    textOutput.removeChild(textoOculto);
+    alert("El texto se ha copiado al portapapeles.");
+    // location.reload();
+})
+
+encryptButton.addEventListener('click', function () {
+    const text = textInput.value.toLowerCase()
     textOutput.innerHTML = encryptText(text)
     console.log(text + '=>' + encryptText(text))
 })
-decryptButton.addEventListener("click", function () {
-    const text = textInput.value
+decryptButton.addEventListener('click', function () {
+    const text = textInput.value.toLowerCase()
     textOutput.innerHTML = decryptText(text)
     console.log(text + '=>' + decryptText(text))
 })
