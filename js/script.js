f7e0249641f0a39825fa1fa8b9fd7cc4e43d8c22
JesusBora <jramirezb2000@gmail.com>
2023-04-12 10:33:53 -0600

    Encriptador/Desencriptador terminado

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const textInput = document.getElementById('text-input')
+const encryptButton = document.getElementById('encrypt')
+const decryptButton = document.getElementById('decrypt')
+const textOutput = document.getElementById('text-modified')
+const copyright = document.getElementById("copyright");
+const caracters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '];
+const currentYear = new Date().getFullYear();
+
+copyright.innerHTML = `&copy; ${currentYear}`;
+
+function encryptText(text) {
+    let result = ''
+    Array.from(text).forEach(letter => {
+        if (letter == '\n') {
+            result += '<br>'
+        } else {
+            result += caracters[(caracters.indexOf(letter.toUpperCase()) + 10) % caracters.length]
+        }
+    })
+    return result
+}
+
+function decryptText(text) {
+    let result = ''
+    Array.from(text).forEach(letter => {
+        if (letter == '\n') {
+            result += '<br>'
+        } else {
+            result += caracters[(caracters.indexOf(letter.toUpperCase()) - 10) % caracters.length]
+        }
+    })
+    return result
+}
+
+encryptButton.addEventListener("click", function () {
+    const text = textInput.value
+    textOutput.innerHTML = encryptText(text)
+})
+decryptButton.addEventListener("click", function () {
+    const text = textInput.value
+    textOutput.innerHTML = decryptText(text)
+})
